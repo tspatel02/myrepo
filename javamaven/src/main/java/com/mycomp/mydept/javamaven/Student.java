93f8ae5a002fd6802120c9778e85d0a6f0bb10a5
Patel <212066255@logon.ds.ge.com>
2017-06-22 20:24:52 -0500

    this is my first project

---
+++ javamaven/src/main/java/com/mycomp/mydept/javamaven/Student.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycomp.mydept.javamaven;
+
+/**
+ *
+ * @author 212066255
+ */
+public class Student {
+    
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + "name=" + name + ", age=" + age + '}';
+    }
+    
+    
+    
+}
